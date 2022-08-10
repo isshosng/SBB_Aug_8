60fd217cf458d36c9b50d16c57cb6a52f899c89d
isshosng <purehug@hanmail.net>
2022-08-10 13:30:11 +0900

    1330

--- src/main/java/com/ll/exam/sbb/Answer.java
+++ src/main/java/com/ll/exam/sbb/Answer.java
@@ -1,16 +1,12 @@
 package com.ll.exam.sbb;
 
-
 import java.time.LocalDateTime;
-import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -18,19 +14,15 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-public class Question {
+public class Answer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
-    @Column(length = 200)
-    private String subject;
 
     @Column(columnDefinition = "TEXT")
     private String content;
 
     private LocalDateTime createDate;
 
-    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
-    private List<Answer> answerList;
+    private Question question;
 }
