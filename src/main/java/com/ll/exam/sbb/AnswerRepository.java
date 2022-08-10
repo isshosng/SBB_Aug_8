44a24678b2a155d4406b9403e352bd8d41098c24
isshosng <purehug@hanmail.net>
2022-08-10 13:42:48 +0900

    1342

--- src/main/java/com/ll/exam/sbb/AnswerRepository.java
+++ src/main/java/com/ll/exam/sbb/AnswerRepository.java
@@ -2,5 +2,6 @@ package com.ll.exam.sbb;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QuestionRepository extends JpaRepository<Question, Integer> {
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+
 }
